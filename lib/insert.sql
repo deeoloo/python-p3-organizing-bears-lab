b270d354c678bd7fa055fbac4ce061145ff85b2f
deenoloo <doreenoloo2@gmail.com>
2025-05-18 07:04:09 -0400

    organizing

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr Chocolate', 2, 'M', 'brown', 'Sanguin',1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 1, 'F', 'white', 'Choleric',0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 3, 'F', 'black', 'Phlegmatic',1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 4, 'M', 'brown', 'Melancholic',1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 3, 'F', 'black', 'Senguin',1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 2, 'M', 'brown', 'Choleric',0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 5, 'F', 'white', 'Senguin',1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, NULL, NULL, NULL, NULL,NULL);
